788e7b2b22bbe4d14d0eb2262d2df2228bb55811
Nguyen Viet Anh <anh2101011@gmail.com>
2023-05-14 19:37:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,39 +7,39 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-
 def main():
+    # Set up Streamlit page
     st.set_page_config(page_title="Ask your PDF")
     st.header("Questions to ask about your book💬")
 
-    # prompt user for OpenAI API key
+    # Prompt user for OpenAI API key
     api_key = st.text_input("Enter your OpenAI API key:")
 
     if not api_key:
         st.warning("Please enter your OpenAI API key before uploading the PDF file and asking a question.")
         return
 
-    # initialize text variable
+    # Initialize text variable
     text = ""
 
-    # upload files
+    # Upload PDF files
     pdfs = st.file_uploader("Upload your books (pdf)", type="pdf", accept_multiple_files=True)
 
-    # extract the text
+    # Extract text from PDF files
     if pdfs is not None:
         all_text = ""
         if isinstance(pdfs, list):
-            # handle multiple files
+            # Handle multiple files
             for pdf in pdfs:
                 pdf_reader = PdfReader(pdf)
                 text = "\n".join([page.extract_text() for page in pdf_reader.pages])
                 all_text += text + "\n"
         else:
-            # handle single file
+            # Handle single file
             pdf_reader = PdfReader(pdfs)
             all_text = "\n".join([page.extract_text() for page in pdf_reader.pages])
 
-        # split into chunks
+        # Split text into chunks for processing
         text_splitter = CharacterTextSplitter(
             separator="\n",
             chunk_size=1000,
@@ -48,18 +48,19 @@ def main():
         )
         chunks = text_splitter.split_text(all_text)
 
-        print("Number of chunks:", len(chunks))
-        print("Chunks:", chunks)
+        # Check if there are any chunks to process
         if not chunks:
-          st.warning("Upload your books (pdf),  can upload and process either a single PDF file or multiple PDF files at once")
-          return
+            st.warning("Please upload a PDF file or multiple PDF files to continue.")
+            return
 
-        # create embeddings
+        # Create embeddings for the text chunks
         embeddings = OpenAIEmbeddings(openai_api_key=api_key)
         knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-        # show user input
-        user_question = st.text_input("Ask a question about your PDF:")
+        # Ask user for question to answer
+        user_question = st.text_input("What would you like to know?")
+
+        # Answer the user's question using the language model
         if user_question:
             docs = knowledge_base.similarity_search(user_question)
 
@@ -68,8 +69,8 @@ def main():
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=user_question)
 
+            # Display the response on the Streamlit page
             st.write(response)
-
 
 if __name__ == '__main__':
     main()
