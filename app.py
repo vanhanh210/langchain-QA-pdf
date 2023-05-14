3c66e1e846e4d62379ce6bb48093da8c9f7034cf
Nguyen Viet Anh <anh2101011@gmail.com>
2023-05-14 16:02:29 +0700

    Published

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+def main():
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Questions to ask about your book💬")
+    
+    # prompt user for OpenAI API key
+    api_key = st.text_input("Enter your OpenAI API key:")
+    
+    if not api_key:
+        st.warning("Please enter your OpenAI API key.")
+        return
+    
+    # upload file
+    pdf = st.file_uploader("Upload your book (pdf)", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI(openai_api_key=api_key)
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
+
+if __name__ == '__main__':
+    main()
